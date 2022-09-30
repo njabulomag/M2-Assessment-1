5ed65c56282ccb7c1191538da06abbddf5c01903
njabulo hlatshwayo <105054827+njabulomag@users.noreply.github.com>
2022-09-30 02:13:37 +0200

    Add files via upload

---
+++ b.js
@@ -0,0 +1,7 @@
+var rollResult = Math.floor(Math.random( )* 6) + 1;
+if (rollResult== 1) console.log ('Consumer Solution');
+else if (rollResult== 2) console.log ('African Solution');
+else if (rollResult== 3)  console.log ('Gaming Solution');
+else if (rollResult== 4) console.log ('Health Solution');
+else if (rollResult== 5) console.log ('Innovative Solution');
+else console.log ('Enterprise Solution');
