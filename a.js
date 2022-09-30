73d6f42814c158168e04f0b0563452856f22c25d
njabulo hlatshwayo <105054827+njabulomag@users.noreply.github.com>
2022-09-30 02:11:40 +0200

    Add files via upload

---
+++ a.js
@@ -0,0 +1,4 @@
+var Name ='Njabulo',
+Institution ='MTn academy' , Githubusername ='Njabulo.mag'
+
+console.log("My name is " + Name , "a student from " + Institution ,"and my Github username is " + Githubusername) 
